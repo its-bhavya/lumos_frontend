094de76cf1fef07706287daaf15d44939afb3e0e
Bhavya <bhavyaaa716@gmail.com>
2025-11-16 16:02:01 +0000

    in the landing page, chage the logo to a nice minimal relvant one

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,10 +12,10 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2a7 7 0 1 0 10 10" />
-      <path d="M12 2v10" />
-      <path d="m16.2 7.8 1.4-1.4" />
-      <path d="m20 12-2-2" />
+      <path d="M15.5 13a.5.5 0 0 1-.5-.5V10a4 4 0 0 0-4-4H7" />
+      <path d="M10 18a3 3 0 0 1-3-3V9" />
+      <path d="M14 20a2 2 0 0 0 2-2 2 2 0 0 0-2-2" />
+      <path d="M7.5 13h1" />
     </svg>
   );
 }
